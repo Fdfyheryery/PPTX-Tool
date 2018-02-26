47596edf412bb7ca1a6ce1b5d6ddeff43ab4c224
Bobby Bernier <bobby.bernier@gmail.com>
2018-02-26 09:37:41 -0500

    Moved main logic into the appropriate class

--- PPTX-Tool.ps1
+++ PPTX-Tool.ps1
@@ -11,6 +11,19 @@ WIP
 
 #>
 
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+function GetEntryAsXML {
+    param([System.IO.Compression.ZipArchiveEntry]$entry)
+
+    $slide = $entry.Open()
+    $reader = New-Object IO.StreamReader($slide)
+    [xml]$entryXML = $reader.ReadToEnd()
+    $reader.Close()
+    $slide.Close()
+    return $entryXML
+}
+
 Class PPTXFile
 {
     [string]$name
@@ -81,11 +94,186 @@ Class PPTXExcel : PPTXFile
 
 Class PPTXPowerPoint : PPTXFile
 {
-    [PPTXImage[]]$images
+    [PPTXFile[]]$arrayImages
+
 
     PPTXPowerPoint ([string]$name)
     {
         $this.name = $name
+        $this.AnalyzeFile()
+    }
+
+    hidden [void] AnalyzeFile() {
+ 
+        $zipArchive = [System.IO.Compression.ZipFile]::OpenRead($this.name)
+
+        #On incrémente et vérifie si la slide existe (pptx commencent à 1)
+        $i = 1
+        $slideExist = $true;
+        while($slideExist -eq $true) {
+        
+            $slidePath = "ppt/slides/slide" + $i + ".xml"
+            $entry = $zipArchive.GetEntry($slidePath)
+
+            if ($entry) {
+                $rIds = $null
+                [PPTXFile[]]$rIds = @()
+
+                $slideContent = GetEntryAsXML $entry
+
+                #Image et Vidéo
+                foreach ($pic in $slideContent.sld.csld.sptree.pic) {
+                    #rID
+                    $rId = $pic.blipfill.blip.embed
+
+                    #Ratio (Image source : Image PPTX)
+                    if ($pic.sppr.xfrm.ext.cx -lt $pic.sppr.xfrm.ext.cy) {
+                        $ratio = $pic.sppr.xfrm.ext.cx / 914400
+                    
+                    }
+                    else {
+                        $ratio = $pic.sppr.xfrm.ext.cy / 914400
+                    }
+
+                    #Utilisation (Rognage) (10000 = 10.000%)
+                    $utilVertical = 100000 - ([int]$pic.blipfill.srcRect.t + [int]$pic.blipfill.srcRect.b)
+                    $utilHorizontal = 100000 - ([int]$pic.blipfill.srcRect.l + [int]$pic.blipfill.srcRect.r)
+
+
+                    if (($rIds.Count -gt 0) -and ($rIds.Name -contains $rId)) {
+                        $index = $rIds.name.indexof($rId)
+                        $rIds[$index].Total++
+
+                        if ($rIds[$index].Ratio -gt $ratio) {
+                            $rIds[$index].Ratio = $ratio
+                        }
+
+                        if ($rIds[$index].UtilisationV -lt $utilVertical) {
+                            $rIds[$index].UtilisationV = $utilVertical
+                        }
+
+                        if ($rIds[$index].UtilisationH -lt $utilHorizontal) {
+                            $rIds[$index].UtilisationH = $utilHorizontal
+                        }
+                    }
+                    else {
+                        $newItem = [PPTXImage]::new($rId)
+                        $newItem.ratio = $ratio
+                        $newItem.utilisationV = $utilVertical
+                        $newItem.utilisationH = $utilHorizontal
+                        $newItem.total = 1
+
+                        $rIds += $newItem
+                    }
+
+                    #Si l'image est un preview de vidéo, on ajoute la vidéo dans la liste
+                    if ($pic.nvpicpr.nvpr.videofile.link -ne $null) {
+                        $rId = $pic.nvpicpr.nvpr.videofile.link
+                        if (($rIds.Count -gt 0) -and ($rIds.Name -contains $rId)) {
+                            $index = $rIds.name.indexof($rId)
+                            $rIds[$index].Total++
+                        }
+                        else {
+                            $newItem = [PPTXVideo]::new($rId)
+                            $newItem.total = 1
+
+                            $rIds += $newItem
+                        }
+                    }
+                }
+
+                #Référence dans le fichier xml.rels 
+                $relsPath = "ppt/slides/_rels/slide" + $i + ".xml.rels"
+                $entry = $zipArchive.GetEntry($relsPath)
+
+                if ($entry) {
+
+                    [xml]$relsContent = GetEntryAsXML $entry
+
+                    #Pour chaque rId: le nom du fichier associée, puis met à jour les informations ou créé l'entrée
+                    for($j=0;$j -lt $rIds.Length;$j++) {
+                        $xmlNode = $relsContent.relationships.Relationship.Where({$_.Id -eq $rIds[$j].name})
+                        $image = $xmlNode.Target.Substring(9)
+
+                        #Images
+                        if ($xmlNode.Type -eq "http://schemas.openxmlformats.org/officeDocument/2006/relationships/image") {
+                            if (($this.arrayImages.Count -gt 0) -and ($this.arrayImages.Name -contains $image)) {
+                                $indexImage = $this.arrayImages.name.indexof($image)
+
+                                $this.arrayImages[$indexImage].Total = $this.arrayImages[$indexImage].Total + $rIds[$j].Total
+                                $this.arrayImages[$indexImage].Slides += $i
+
+                                if ($this.arrayImages[$indexImage].Ratio -gt $rIds[$j].ratio) {
+                                    $this.arrayImages[$indexImage].Ratio = $rIds[$j].Ratio
+                                }
+
+                                if ($this.arrayImages[$indexImage].utilisationV -lt $rIds[$j].utilisationV) {
+                                    $this.arrayImages[$indexImage].utilisationV = $rIds[$j].utilisationV
+                                }
+
+                                if ($this.arrayImages[$indexImage].utilisationH -lt $rIds[$j].utilisationH) {
+                                    $this.arrayImages[$indexImage].utilisationH = $rIds[$j].utilisationH
+                                }
+                            }
+                            else {
+                                $newItem = [PPTXImage]::new($image)
+                                $newItem.ratio = $rIds[$j].ratio
+                                $newItem.utilisationV = $rIds[$j].utilisationV
+                                $newItem.utilisationH = $rIds[$j].utilisationH
+                                $newItem.slides = @($i)
+                                $newItem.total = $rIds[$j].total
+                                $this.arrayImages += $newItem
+                            }
+                        }
+
+                        #Videos
+                        elseif ($xmlNode.Type -eq "http://schemas.openxmlformats.org/officeDocument/2006/relationships/video") {
+                            if (($this.arrayImages.Count -gt 0) -and ($this.arrayImages.Name -contains $image)) {
+                                $indexImage = $this.arrayImages.name.indexof($image)
+
+                                $this.arrayImages[$indexImage].Total = $this.arrayImages[$indexImage].Total + $rIds[$j].Total
+                                $this.arrayImages[$indexImage].Slides += $i
+                            }
+                            else {
+                                $newItem = [PPTXVideo]::new($image)
+                                $newItem.slides = @($i)
+                                $newItem.total = $rIds[$j].total
+                                $this.arrayImages += $newItem
+                            }
+                        }
+                    }  
+                }
+
+                else {
+                    #Normalement il y a toujours un fichier .rels d'associé à une diapositive
+                    $errorMsg = "Erreur: Fichier " + $relsPath + " introuvable."
+                    Write-Host $errorMsg
+                }
+            }
+
+            else {
+                $slideExist = $false
+            }
+            $i++
+        }
+
+        foreach ($file in $this.arrayImages) {
+            $filePath = ""
+
+            if ($file.GetType().Name -eq "PPTXImage") {
+                $filePath = "ppt/media/" + $file.Name
+            }
+
+            elseif ($file.GetType().Name -eq "PPTXVideo") {
+                $filePath = "ppt/media/" + $file.Name
+            }
+
+            $entry = $zipArchive.GetEntry($filePath)
+            $file.filesize = $entry.Length
+            $hasWarning = $file.CreateWarning()
+        }
+
+        $zipArchive.Dispose()
     }
 
     [bool]CreateWarning()
@@ -117,202 +305,6 @@ Class PPTXWord : PPTXFile
     }
 }
 
-function GetEntryAsXML {
-    param([System.IO.Compression.ZipArchiveEntry]$entry)
-
-    $slide = $entry.Open()
-    $reader = New-Object IO.StreamReader($slide)
-    [xml]$entryXML = $reader.ReadToEnd()
-    $reader.Close()
-    $slide.Close()
-    return $entryXML
-}
-
-function FindUsedImages {
-    param([string]$filename)
-
-    [PPTXFile[]]$arrayImages = @()
-
-    Add-Type -AssemblyName System.IO.Compression.FileSystem
-    $zipArchive = [System.IO.Compression.ZipFile]::OpenRead($filename)
-
-    #On incrémente et vérifie si la slide existe (pptx commencent à 1)
-    $i = 1
-    $slideExist = $true;
-    while($slideExist -eq $true) {
-        
-        $slidePath = "ppt/slides/slide" + $i + ".xml"
-        $entry = $zipArchive.GetEntry($slidePath)
-
-        if ($entry) {
-            $rIds = $null
-            [PPTXFile[]]$rIds = @()
-
-            $slideContent = GetEntryAsXML $entry
-
-            #Va chercher le ratio et rId pour chaque image de la slide
-            foreach ($pic in $slideContent.sld.csld.sptree.pic) {
-                #rID
-                $rId = $pic.blipfill.blip.embed
-
-                #Ratio (Image source : Image PPTX)
-                if ($pic.sppr.xfrm.ext.cx -lt $pic.sppr.xfrm.ext.cy) {
-                    $ratio = $pic.sppr.xfrm.ext.cx / 914400
-                    
-                }
-                else {
-                    $ratio = $pic.sppr.xfrm.ext.cy / 914400
-                }
-
-                #Utilisation (Rognage) (10000 = 10.000%)
-                $utilVertical = 100000 - ([int]$pic.blipfill.srcRect.t + [int]$pic.blipfill.srcRect.b)
-                $utilHorizontal = 100000 - ([int]$pic.blipfill.srcRect.l + [int]$pic.blipfill.srcRect.r)
-
-
-                if (($rIds.Count -gt 0) -and ($rIds.Name -contains $rId)) {
-                    $index = $rIds.name.indexof($rId)
-                    $rIds[$index].Total++
-
-                    if ($rIds[$index].Ratio -gt $ratio) {
-                        $rIds[$index].Ratio = $ratio
-                    }
-
-                    if ($rIds[$index].UtilisationV -lt $utilVertical) {
-                        $rIds[$index].UtilisationV = $utilVertical
-                    }
-
-                    if ($rIds[$index].UtilisationH -lt $utilHorizontal) {
-                        $rIds[$index].UtilisationH = $utilHorizontal
-                    }
-                }
-                else {
-                    $newItem = [PPTXImage]::new($rId)
-                    $newItem.ratio = $ratio
-                    $newItem.utilisationV = $utilVertical
-                    $newItem.utilisationH = $utilHorizontal
-                    $newItem.total = 1
-
-                    $rIds += $newItem
-                }
-
-                #Si l'image est un preview de vidéo, on ajoute la vidéo dans la liste
-                if ($pic.nvpicpr.nvpr.videofile.link -ne $null) {
-                    $rId = $pic.nvpicpr.nvpr.videofile.link
-                    if (($rIds.Count -gt 0) -and ($rIds.Name -contains $rId)) {
-                        $index = $rIds.name.indexof($rId)
-                        $rIds[$index].Total++
-                    }
-                    else {
-                        $newItem = [PPTXVideo]::new($rId)
-                        $newItem.total = 1
-
-                        $rIds += $newItem
-                    }
-                }
-            }
-
-            #Va chercher la bonne référence dans le fichier xml.rels 
-            $relsPath = "ppt/slides/_rels/slide" + $i + ".xml.rels"
-            $entry = $zipArchive.GetEntry($relsPath)
-
-            if ($entry) {
-
-                [xml]$relsContent = GetEntryAsXML $entry
-
-                #Va chercher, pour chaque rId, le nom de l'image associée, puis met à jour les informations (ou ajoute l'entrée si non-existant)
-                for($j=0;$j -lt $rIds.Length;$j++) {
-                    $xmlNode = $relsContent.relationships.Relationship.Where({$_.Id -eq $rIds[$j].name})
-                    $image = $xmlNode.Target.Substring(9)
-
-                    #Images
-                    if ($xmlNode.Type -eq "http://schemas.openxmlformats.org/officeDocument/2006/relationships/image") {
-                        if (($arrayImages.Count -gt 0) -and ($arrayImages.Name -contains $image)) {
-                            $indexImage = $arrayImages.name.indexof($image)
-
-                            $arrayImages[$indexImage].Total = $arrayImages[$indexImage].Total + $rIds[$j].Total
-                            $arrayImages[$indexImage].Slides += $i
-
-                            if ($arrayImages[$indexImage].Ratio -gt $rIds[$j].ratio) {
-                                $arrayImages[$indexImage].Ratio = $rIds[$j].Ratio
-                            }
-
-                            if ($arrayImages[$indexImage].utilisationV -lt $rIds[$j].utilisationV) {
-                                $arrayImages[$indexImage].utilisationV = $rIds[$j].utilisationV
-                            }
-
-                            if ($arrayImages[$indexImage].utilisationH -lt $rIds[$j].utilisationH) {
-                                $arrayImages[$indexImage].utilisationH = $rIds[$j].utilisationH
-                            }
-                        }
-                        else {
-                            $newItem = [PPTXImage]::new($image)
-                            $newItem.ratio = $rIds[$j].ratio
-                            $newItem.utilisationV = $rIds[$j].utilisationV
-                            $newItem.utilisationH = $rIds[$j].utilisationH
-                            $newItem.slides = @($i)
-                            $newItem.total = $rIds[$j].total
-                            $arrayImages += $newItem
-                        }
-                    }
-
-                    #Videos
-                    if ($xmlNode.Type -eq "http://schemas.openxmlformats.org/officeDocument/2006/relationships/video") {
-                        if (($arrayImages.Count -gt 0) -and ($arrayImages.Name -contains $image)) {
-                            $indexImage = $arrayImages.name.indexof($image)
-
-                            $arrayImages[$indexImage].Total = $arrayImages[$indexImage].Total + $rIds[$j].Total
-                            $arrayImages[$indexImage].Slides += $i
-                        }
-                        else {
-                            $newItem = [PPTXVideo]::new($image)
-                            $newItem.slides = @($i)
-                            $newItem.total = $rIds[$j].total
-                            $arrayImages += $newItem
-                        }
-                    }
-                }
-
-                
-            }
-
-            else {
-                #Normalement il y a toujours un fichier .rels d'associé à une diapositive
-                $errorMsg = "Erreur: Fichier " + $relsPath + " introuvable."
-                Write-Host $errorMsg
-            }
-        }
-
-        else {
-            $slideExist = $false
-        }
-        $i++
-    }
-    $zipArchive.Dispose()
-    return $arrayImages
-}
-
-function GenerateWarnings {
-    param([string]$filename, [PPTXFile[]]$fileArray)
-
-    Add-Type -AssemblyName System.IO.Compression.FileSystem
-    $zipArchive = [System.IO.Compression.ZipFile]::OpenRead($filename)
-
-    foreach ($file in $fileArray) {
-        if ($file.GetType().Name -eq "PPTXImage") {
-            $filePath = "ppt/media/" + $file.Name
-        }
-        if ($file.GetType().Name -eq "PPTXVideo") {
-            $filePath = "ppt/media/" + $file.Name
-        }
-        $entry = $zipArchive.GetEntry($filePath)
-        $file.filesize = $entry.Length
-        $hasWarning = $file.CreateWarning()
-    }
-
-    $zipArchive.Dispose()
-    return $fileArray
-}
-
 #Ouvre une fenêtre pour la sélection du fichier PowerPoint
 Add-Type -AssemblyName System.Windows.Forms
 $openFileDialog = New-Object Windows.Forms.OpenFileDialog
@@ -322,10 +314,9 @@ $result = $openFileDialog.ShowDialog()
 
 if (($result -eq "OK") -and $openFileDialog.CheckFileExists) {
 
-    $images = FindUsedImages -filename $openFileDialog.FileName
-
-    $images = GenerateWarnings -filename $openFileDialog.FileName -fileArray $images
+    [PPTXPowerPoint]$analyzedFile = [PPTXPowerPoint]::new($openFileDialog.FileName)
 
     #Affichage temporaire
-    $images | Where-Object {$_.warning}
+    $analyzedFile.arrayImages | Where-Object {$_.warning}
+    
 }
